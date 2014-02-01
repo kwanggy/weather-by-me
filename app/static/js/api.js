1fc554fd3a37cca92d8b86ea9debdf9691b7d4b4
Junwoo Kim <disciple1224@gmail.com>
2014-02-01 05:58:22 -0500

    signup implemented not tested

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -1,20 +1,18 @@
-function signup(email, pw, name, callback) {
-	$.post("api/signup",
-	{
-		'email': email,
-    	'pw': pw,
-    	'name': name
-  	},
-  	function(data, status){
-  		if (data['status_code'] == 200) {
+function signup(form_id) {
+	var formData = new FormData($(image_form_id)[0]);
+	$.ajax({"api/post",
+			type: 'POST',
+			data: formData,
+            contentType: false,
+            processData: false
+    }).done(function(data, status) {
+    	if (data['status_code'] == 200) {
   			top.session_key = data['session_key']
-
   			callback(true);
   		} else {
   			callback(false)
   		}
-    	// alert("Data: " + data['error'] + "\nStatus: " + status);
-  	});
+    });
 }
 
 function signin(email, pw, callback) {
@@ -34,3 +32,32 @@ function signin(email, pw, callback) {
   	});	
 }
 
+function post(form_id) {
+	var formData = new FormData($(form_id)[0]);
+	$.ajax({"api/post",
+			type: 'POST',
+			data: formData,
+            contentType: false,
+            processData: false
+    }).done(function( data ) {
+
+    });
+}
+
+
+	// $.post("api/signup",
+	// {
+	// 	'email': email,
+ //    	'pw': pw,
+ //    	'name': name
+ //  	},
+ //  	function(data, status){
+ //  		if (data['status_code'] == 200) {
+ //  			top.session_key = data['session_key']
+
+ //  			callback(true);
+ //  		} else {
+ //  			callback(false)
+ //  		}
+ //    	// alert("Data: " + data['error'] + "\nStatus: " + status);
+ //  	});
